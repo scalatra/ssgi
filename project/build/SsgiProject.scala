f50cae8bb2487faf7fb57255b0d3d0d1d27ddef1
Ross A. Baker <baker@alumni.indiana.edu>
2010-10-26 13:55:48 -0400

    Split into core and servlet projects.

--- project/build/SsgiProject.scala
+++ project/build/SsgiProject.scala
@@ -1,5 +1,18 @@
 import sbt._
 
 class SsgiProject(info: ProjectInfo) extends DefaultProject(info) {
-  val scalatest = "org.scalatest" % "scalatest" % "1.2" % "test"
+  lazy val core = project("core", "core", new CoreProject(_))
+  class CoreProject(info: ProjectInfo) extends DefaultProject(info) with SsgiSubProject {
+    val servletApi = "javax.servlet" % "servlet-api" % "2.5" % "provided"
+  }
+
+  lazy val servlet = project("servlet", "servlet", new ServletProject(_), core)
+  class ServletProject(info: ProjectInfo) extends DefaultProject(info) with SsgiSubProject {
+    val servletApi = "javax.servlet" % "servlet-api" % "2.5" % "provided"
+  }
+
+  trait SsgiSubProject {
+    this: BasicScalaProject =>
+    val scalatest = "org.scalatest" % "scalatest" % "1.2" % "test"
+  }
 }
