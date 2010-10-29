2b7525a4a27bac2d1e21bf844b15a4918e1c1c0d
Ross A. Baker <baker@alumni.indiana.edu>
2010-10-29 14:12:06 -0400

    A response body must be viewable as a Renderable.

---
+++ core/src/main/scala/org/scalatra/ssgi/Renderable.scala
@@ -0,0 +1,14 @@
+package org.scalatra.ssgi
+
+import scala.xml.NodeSeq
+import java.io.{File, InputStream}
+
+trait Renderable
+
+object Renderable {
+  implicit def byteTraversableToRenderable(bytes: Traversable[Byte]) = new Renderable {}
+  implicit def stringToRenderable(string: String) = new Renderable {}
+  implicit def nodeSeqToRenderable(nodeSeq: NodeSeq) = new Renderable {}
+  implicit def inputStreamToRenderable(in: InputStream) = new Renderable {}
+  implicit def fileToRenderable(file: File) = new Renderable {}
+}
