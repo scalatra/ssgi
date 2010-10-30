33d4a4f656b57e52dd5f6fbaf9e522b1faa8d598
Ross A. Baker <baker@alumni.indiana.edu>
2010-10-29 21:40:49 -0400

    An Array[Byte] is renderable.

--- core/src/main/scala/org/scalatra/ssgi/Renderable.scala
+++ core/src/main/scala/org/scalatra/ssgi/Renderable.scala
@@ -7,6 +7,7 @@ trait Renderable
 
 object Renderable {
   implicit def byteTraversableToRenderable(bytes: Traversable[Byte]) = new Renderable {}
+  implicit def byteArrayToRenderable(bytes: Array[Byte]) = new Renderable {}
   implicit def stringToRenderable(string: String) = new Renderable {}
   implicit def nodeSeqToRenderable(nodeSeq: NodeSeq) = new Renderable {}
   implicit def inputStreamToRenderable(in: InputStream) = new Renderable {}
