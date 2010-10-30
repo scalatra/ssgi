9e9bfbed943342091103a7b6eed80e88ebe9f2bb
Ross A. Baker <baker@alumni.indiana.edu>
2010-10-29 21:46:09 -0400

    Implicitly create an Application from a similarly typed function.

--- core/src/main/scala/org/scalatra/ssgi/Application.scala
+++ core/src/main/scala/org/scalatra/ssgi/Application.scala
@@ -4,3 +4,7 @@ package org.scalatra.ssgi
  * An application is a function that takes exactly one argument, a request, and returns a response.
  */
 trait Application[+A] extends (Request => Response[A])
+
+object Application {
+  implicit def apply[A](f: Request => Response[A]) = new Application[A]{ def apply(req: Request) = f(req) }
+}
