1bb4afd9cdd1095215a8dc9e776aaec95d2cd402
Ross A. Baker <baker@alumni.indiana.edu>
2010-11-02 10:48:05 -0400

    Add ssgi- prefix to artifact names.

--- project/build/SsgiProject.scala
+++ project/build/SsgiProject.scala
@@ -1,21 +1,21 @@
 import sbt._
 
 class SsgiProject(info: ProjectInfo) extends DefaultProject(info) {
-  lazy val core = project("core", "core", new CoreProject(_))
+  lazy val core = project("core", "ssgi-core", new CoreProject(_))
   class CoreProject(info: ProjectInfo) extends DefaultProject(info) with SsgiSubProject {
     val servletApi = "javax.servlet" % "servlet-api" % "2.5" % "provided"
     val commonsLang =  "commons-lang" % "commons-lang" % "2.5"
     val mimeUtil = "eu.medsea.mimeutil" % "mime-util" % "2.1.3"
   }
 
-  lazy val servlet = project("servlet", "servlet", new ServletProject(_), core)
+  lazy val servlet = project("servlet", "ssgi-servlet", new ServletProject(_), core)
   class ServletProject(info: ProjectInfo) extends DefaultProject(info) with SsgiSubProject {
     val servletApi = "javax.servlet" % "servlet-api" % "2.5" % "provided" withSources
   }
 
-  lazy val examples = project("examples", "example", new Examples(_))
+  lazy val examples = project("examples", "ssgi-example", new Examples(_))
   class Examples(info: ProjectInfo) extends ParentProject(info) {
-    val servletExample = project("servlet", "servlet-example", new ServletExampleProject(_), servlet)
+    val servletExample = project("servlet", "ssgi-servlet-example", new ServletExampleProject(_), servlet)
     class ServletExampleProject(info: ProjectInfo) extends DefaultWebProject(info) with SsgiSubProject {
       val jetty6 = "org.mortbay.jetty" % "jetty" % "6.1.25" % "test"
     }
