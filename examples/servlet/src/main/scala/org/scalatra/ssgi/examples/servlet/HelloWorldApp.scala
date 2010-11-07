53cd67a91e20a4015388f0200b52fb335feeb06c
Ivan Porto Carrero <ivan@flanders.co.nz>
2010-11-07 01:27:51 +0000

    Adds example

--- examples/servlet/src/main/scala/org/scalatra/ssgi/examples/servlet/HelloWorldApp.scala
+++ examples/servlet/src/main/scala/org/scalatra/ssgi/examples/servlet/HelloWorldApp.scala
@@ -3,6 +3,13 @@ package examples.servlet
 
 import scala.xml.NodeSeq
 
-class HelloWorldApp extends Application {
-  def apply(v1: Request) = Response(body = <h1>Hello, world!</h1>)
+trait BodyUppercaser extends Middleware { self: Application => 
+  abstract override def apply(v1: Request): Response[_] = {
+    val resp = super.apply(v1)
+    resp.copy(body = <h1>(resp.body \ "h1").text.toUpperCase</h1>)
+  }
+}
+
+class HelloWorldApp extends Application with BodyUppercaser {
+  def apply(v1: Request): Response[_] = Response(body = <h1>Hello, world!</h1>)
 }
